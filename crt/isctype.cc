6040cb6206c937dbde0d994660cea0361d1e990f
Pavel P <pavlov.pavel@gmail.com>
2017-07-19 00:44:40 -0700

    declare missing _pctype for VS2015

--- crt/isctype.cc
+++ crt/isctype.cc
@@ -5,6 +5,10 @@
 #include "libctiny.h"
 #include <ctype.h>
 
+#if _MSC_VER >= 1900
+extern const unsigned short* _pctype;
+#endif
+
 extern "C" int __cdecl _isctype( int c , int mask ) {
     /* c valid between -1 and 255 */
     if (((unsigned)(c + 1)) <= 256)
